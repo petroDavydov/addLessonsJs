1aff5c05d06a1fe6ceafd0fd61ef9505e9d93702
Evgeniya Bayda <baje75128@gmail.com>
2021-06-19 17:47:25 +0300

    пуш

--- main.js
+++ main.js
@@ -205,35 +205,14 @@
 // ); // lux
 
 
-// / Напиши функцию getAllPropValues(arr, prop), которая получает массив объектов и имя свойства. Возвращает массив значений определенного свойства prop из каждого объекта в массиве.
-
-// const products = [
-//     { name: 'Радар', price: 1300, quantity: 4 },
-//     { name: 'Сканер', price: 2700, quantity: 3 },
-//     { name: 'Дроид', price: 400, quantity: 7 },
-//     { name: 'Захват', price: 1200, quantity: 2 },
-// ];
-
-// const getAllPropValues = function (arr, prop) {
-//     // твой код
-//     let newArr = [];
-//     for (const product of products) {
-//         if (prop in product) {
-//             newArr.push(product[prop]);
-//         }
-//     }
-//     return newArr;
-// };
-
-// /*
-//  * Вызовы функции для проверки работоспособности твоей реализации.
-//  */
-// console.log(getAllPropValues(products, 'name')); // ['Радар', 'Сканер', 'Дроид', 'Захват']
-
-// console.log(getAllPropValues(products, 'quantity')); // [4, 3, 7, 2]
-
-// console.log(getAllPropValues(products, 'category')); // []
-
-
-
-
+// Напиши функцию calculateTotalPrice(allProdcuts, productName), которая получает массив объектов и имя продукта(значение свойства name).Возвращает общую стоимость продукта(цена * количество).
+// // Вызовы функции для проверки работоспособности твоей реализации.  
+// const products = [{ name: 'Радар', price: 1300, quantity: 4 },
+// 	{ name: 'Сканер', price: 2700, quantity: 3 },
+// 	{ name: 'Дроид', price: 400, quantity: 7 }, { name: 'Захват', price: 1200, quantity: 2 },];
+// const calculateTotalPrice = function (allProdcuts, productName) {
+// 	твой код   let totalPrice = 0; for (const product of allProdcuts) {
+// 		if (product.name === productName) { return product.price * product.quantity; }
+// 	}
+// };  /*  * Вызовы функции для проверки работоспособности твоей реализации.  */ console.log(calculateTotalPrice(products, 'Радар'));
+// // 5200  console.log(calculateTotalPrice(products, 'Дроид')); // 2800
